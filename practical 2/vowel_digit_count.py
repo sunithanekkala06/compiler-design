d44a6610d2051e9800f638f9f41a43644a0664c0
YourName <your_email@gmail.com>
2026-03-29 11:10:21 +0530

    Added Practical 2 Python program

---
+++ practical 2/vowel_digit_count.py
@@ -0,0 +1,20 @@
+# Practical 2: Count vowels, consonants, and digits
+
+string = input("Enter a string: ")
+
+vowels = 0
+consonants = 0
+digits = 0
+
+for ch in string:
+    if ch.isalpha():  # checks letters
+        if ch.lower() in "aeiou":
+            vowels += 1
+        else:
+            consonants += 1
+    elif ch.isdigit():  # checks digits
+        digits += 1
+
+print("Vowels:", vowels)
+print("Consonants:", consonants)
+print("Digits:", digits)
